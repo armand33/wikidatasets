305a587761505b8c0ba50a77edebe3827e2201ee
armand33 <aboschin@pm.me>
2019-04-12 19:10:54 +0200

    added script to extract labels dict

---
+++ labelsdict.py
@@ -0,0 +1,6 @@
+from utils import get_labels_dict
+
+path = '/home/aboschin/datasets/wikidata/'
+path_pickle = '/home/aboschin/datasets/wikidata/'
+n_lines = 50000000
+get_labels_dict(path, path_pickle, n_lines)
