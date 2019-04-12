26c1073d0e869f8239d38f4d3d6cf21bcc358e6c
armand33 <aboschin@pm.me>
2019-04-12 19:10:39 +0200

    Added scripts for various datasets

---
+++ dumps-humans.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from utils import query_wikidata_dump, clean
+
+path = '/home/aboschin/datasets/wikidata/'
+path_pickle = '/home/aboschin/datasets/wikidata/humans/'
+n_lines = 50000000
+tails = pd.read_csv('/home/aboschin/datasets/wikidata/subclasses/subclasses_humans.tsv', sep='\t')['item'].apply(clean).values
+
+query_wikidata_dump(path, path_pickle, n_lines, query_tails=tails)
