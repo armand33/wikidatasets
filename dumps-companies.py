ad5e54e827c7d0518cd5f1acf8145afade3890ff
armand33 <aboschin@pm.me>
2019-04-15 13:29:13 +0200

    Update number of lines to prevent overflow

--- dumps-companies.py
+++ dumps-companies.py
@@ -3,7 +3,7 @@ from utils import query_wikidata_dump, clean
 
 path = '/home/aboschin/datasets/wikidata/'
 path_pickle = '/home/aboschin/datasets/wikidata/companies/'
-n_lines = 50000000
+n_lines = 70000000
 tails = pd.read_csv('/home/aboschin/datasets/wikidata/subclasses/subclasses_companies.tsv', sep='\t')['item'].apply(clean).values
 
 query_wikidata_dump(path, path_pickle, n_lines, query_tails=tails)
--- labelsdict.py
+++ labelsdict.py
@@ -2,5 +2,5 @@ from utils import get_labels_dict
 
 path = '/home/aboschin/datasets/wikidata/'
 path_pickle = '/home/aboschin/datasets/wikidata/'
-n_lines = 50000000
+n_lines = 70000000
 get_labels_dict(path, path_pickle, n_lines)
