a30250b83814f01ca384b501fc756acd5a5f575f
armand33 <aboschin@pm.me>
2019-04-16 11:59:03 +0200

    Create build_films.py

---
+++ examples/build_films.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pickle
+from utils import clean
+from processFunctions import query_wikidata_dump, build_dataset
+
+
+path = '/home/aboschin/datasets/wikidata/films/'
+dump_path = '/home/aboschin/datasets/wikidata/latest-all.json.bz2'
+n_lines = 70000000
+tails = pd.read_csv('/home/aboschin/datasets/wikidata/subclasses/subclasses_films.tsv',
+                    sep='\t')['item'].apply(clean).values
+
+query_wikidata_dump(dump_path, path, n_lines, test_entities=tails, collect_labels=False)
+
+labels = pickle.load(open('/home/aboschin/datasets/wikidata/labels.pkl', 'rb'))
+build_dataset(path, labels)
