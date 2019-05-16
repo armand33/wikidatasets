4d90579244ba80f51c8000edc1c6005443d90bad
armand33 <aboschin@pm.me>
2019-05-16 09:23:59 +0200

    Updated entity used for animals.

--- examples/build_animals.py
+++ examples/build_animals.py
@@ -6,7 +6,8 @@ path = '/home/aboschin/datasets/wikidata/animals/'
 dump_path = '/home/public/wikidata/latest-all.json.bz2'
 n_lines = 56208653
 
-test_entities = get_subclasses('Q729')  # kingdom of multicellular eukaryotic organisms
+test_entities = get_subclasses('Q16521')  # group of one or more organism(s), which a taxonomist
+                                          # adjudges to be a unit
 
 query_wikidata_dump(dump_path, path, n_lines, test_entities=test_entities, collect_labels=False)
 
