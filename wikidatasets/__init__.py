d15f304158080a624722ed0344adfcf5e0a09e78
Armand Boschin <aboschin@pm.me>
2020-02-10 15:30:13 +0100

    Bump version: 0.2.2 → 0.2.3

--- wikidatasets/__init__.py
+++ wikidatasets/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Armand Boschin"""
 __email__ = 'aboschin@enst.fr'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 from .processFunctions import get_subclasses
 from .processFunctions import query_wikidata_dump
