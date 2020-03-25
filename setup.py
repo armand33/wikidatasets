bc46d310f27a154b46b1ba387da7c967d248f846
Armand Boschin <aboschin@pm.me>
2020-03-25 16:47:34 +0100

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/armand33/wikidatasets',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
