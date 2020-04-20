a094791a268f03d58c6a0c19665e040f122a8fac
armand <aboschin@pm.me>
2020-04-20 11:26:07 +0200

    Bump version: 0.2.5 → 0.3.0

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/armand33/wikidatasets',
-    version='0.2.5',
+    version='0.3.0',
     zip_safe=False,
 )
--- wikidatasets/__init__.py
+++ wikidatasets/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Armand Boschin"""
 __email__ = 'aboschin@enst.fr'
-__version__ = '0.2.5'
+__version__ = '0.3.0'
 
 from .processFunctions import get_subclasses
 from .processFunctions import query_wikidata_dump
