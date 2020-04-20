65588345aeca0cb25b8346a45867c776b23b2d9f
armand <aboschin@pm.me>
2020-04-20 11:23:24 +0200

    Updated examples

--- wikidatasets/examples/build_animals.py
+++ wikidatasets/examples/build_animals.py
@@ -4,11 +4,11 @@ from wikidatasets.processFunctions import get_subclasses, query_wikidata_dump, b
 # change the 4 following values to match your installation
 path = '../animals/'  # this will contain the files output through the process
 dump_path = 'latest-all.json.bz2'  # path to the bz2 dump file
-n_lines = 56208653  # this can be an upper bound
+n_lines = 81933324  # this can be an upper bound
 test_entities = get_subclasses('Q16521')  # group of one or more organism(s), which a taxonomist
                                           # adjudges to be a unit
 
 query_wikidata_dump(dump_path, path, n_lines, test_entities=test_entities, collect_labels=False)
 
 labels = pickle.load(open(path + 'labels.pkl', 'rb'))
-build_dataset(path, labels)
+build_dataset(path, labels, dump_date='April 15, 2020')
